6a9a4377121cc31214f684eca37f44b40ebc7b97
dollygupta07 <dolly.mailbox.gupta@gmail.com>
2020-07-13 23:12:47 +0530

    first commit

---
+++ cpp/tokens_operators/1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    
+int main()
+{
+	int i;
+	char c;
+	float f;
+	double d;
+
+	cout << "Size of char: " << sizeof(c) <<" bytes\n";
+	cout << "Size of int: " << sizeof(i) <<" bytes\n";
+	cout << "Size of float: " << sizeof(f) <<" bytes\n";
+	cout << "Size of double: " << sizeof(d) <<" bytes\n";
+
+    return 0;
+  
+}
---
+++ cpp/tokens_operators/2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    
+int main()
+{
+	float num1 , num2;
+	cout <<"Enter two numbers:" ;
+	cin >> num1 >> num2;
+	cout <<"Product = " << num1*num2 <<"\n";
+
+    return 0;
+  
+}
